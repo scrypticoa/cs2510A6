a7c8bc47ec882c83ca156a7e2091a0aae60446e3
scrypticoa <scrypticoa@gmail.com>
2024-10-22 19:40:28 -0400

    more

--- src/Visitors.java
+++ src/Visitors.java
@@ -184,17 +184,3 @@ class MirrorVisitor implements IArithVisitor<IArith> {
     return new BinaryFormula (arith.func, arith.name, arith.right, arith.left);
   }
 }
-
-class MirrorVisitor implements IArithVisitor<IArith> {
-  public IArith apply(Const arith) {
-    return arith;
-  }
-  
-  public IArith apply(UnaryFormula arith) {
-    return arith;
-  }
-
-  public IArith apply(BinaryFormula arith) {
-    return new BinaryFormula (arith.func, arith.name, arith.right, arith.left);
-  }
-}
