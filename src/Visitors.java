193f5d9525d4119052e90de5299c472e938c0ae6
scrypticoa <scrypticoa@gmail.com>
2024-10-22 19:02:18 -0400

    push

--- src/Visitors.java
+++ src/Visitors.java
@@ -7,12 +7,19 @@ interface IArith {
 class Const implements IArith {
   double num;
   
+  public <R>R accept(IArithVisitor<R> visitor) {
+    return visitor.apply(this);
+  }
 }
 
 class UnaryFormula implements IArith {
   Function<Double, Double> func;
   String name;
   IArith child;
+  
+  public <R>R accept(IArithVisitor<R> visitor) {
+    return visitor.apply(this);
+  }
 }
 
 class BinaryFormula implements IArith {
@@ -20,6 +27,27 @@ class BinaryFormula implements IArith {
   String name;
   IArith left;
   IArith right;
+  
+  public BinaryFormula(
+      BiFunction <Double, Double, Double> func,
+      String name, IArith left, IArith right) {
+    this.func = func;
+    this.name = name;
+    this.left = left;
+    this.right = right;
+  }
+  
+  public <R>R accept(IArithVisitor<R> visitor) {
+    return visitor.apply(this);
+  }
+}
+
+class Addition extends BinaryFormula {
+  Addition(IArith left, IArith right) {
+    super((a, b) -> {
+      return a + b;
+    }, "plus", left, right);
+  }
 }
 
 interface IArithVisitor<R> {
