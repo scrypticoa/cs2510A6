13a5ed431272f5c5efbee285957f3d77f2b4904e
scrypticoa <scrypticoa@gmail.com>
2024-10-22 20:03:17 -0400

    more

--- src/registrar.java
+++ src/registrar.java
@@ -1,3 +1,21 @@
-interface IList<T> {
+interface IList<T> {}
+
+class MtLoList<T> {
+  T first;
+  IList<T> rest;
   
+  public MtLoList(T first, IList<T> rest) {
+    this.first = first;
+    this.rest = rest;
+  }
+}
+
+class ConsLoList<T> {
+  T first;
+  IList<T> rest;
+  
+  public ConsLoList(T first, IList<T> rest) {
+    this.first = first;
+    this.rest = rest;
+  }
 }
